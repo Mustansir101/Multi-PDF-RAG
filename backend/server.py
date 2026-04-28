8101a5b61ab30180cdbf74bc75cbb0658e9b7459
Mustansir101 <agasi.mustansir@gmail.com>
2026-04-28 15:03:37 +0530

    cors

--- backend/server.py
+++ backend/server.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://multi-pdf-rag-xb4h.onrender.com/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
